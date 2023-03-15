f225d80a604f45bdcba5d2a29a1497ad086d1952
AFaqihAGAFaqihAG <a_faqih@mhs.unsyiah.ac.id>
2023-03-15 13:15:23 +0700

    adding buat_roster_baru activity

---
+++ app/src/main/java/com/example/classschedule/BuatRosterBaru.java
@@ -0,0 +1,12 @@
+package com.example.classschedule;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BuatRosterBaru extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buat_roster_baru);
+    }
+}
